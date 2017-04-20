9aa13fcc9b95d5bbfde227ae5822c4704234ffd9
cbpark <cbpark@gmail.com>
2017-04-20 18:07:49 +0900

    Add hadrons.h, update ppZGmumu.cc to calculate the cross section

---
+++ src/hadrons.h
@@ -0,0 +1,70 @@
+#ifndef SRC_HADRONS_H_
+#define SRC_HADRONS_H_
+
+#include <cmath>
+#include "utils.h"
+
+namespace pmc {
+enum class Qtype { UP, DOWN };
+
+class InitQuark {
+private:
+    double x1_, x2_;
+    double ymax_;
+
+public:
+    InitQuark() = delete;
+    explicit InitQuark(const double s, const double hats) {
+        ymax_ = -0.5 * std::log(hats / s);
+        const double y = (2 * getRandom() - 1.0) * ymax_;
+        const double tau = hats / s;
+        x1_ = std::sqrt(tau) * std::exp(y);
+        x2_ = std::sqrt(tau) * std::exp(-y);
+    }
+
+    double x1() const { return x1_; }
+    double x2() const { return x2_; }
+    double delta_y() const { return 2 * ymax_; }
+};
+
+class Rho {
+private:
+    double rho1_, rho2_;
+
+    double mtr_, gtr_, s_;
+
+public:
+    Rho() = delete;
+    Rho(const double qmin, const double mtr, const double gtr, const double s)
+        : mtr_(mtr), gtr_(gtr), s_(s) {
+        const double gm = gtr_ * mtr_;
+        rho1_ = std::atan((qmin * qmin - mtr_ * mtr_) / gm);
+        rho2_ = std::atan((s_ - mtr_ * mtr_) / gm);
+    }
+
+    double s() const { return s_; }
+
+    double delta() const { return rho2_ - rho1_; }
+
+    double jacobian(const double val) const {
+        const double cosrho = std::cos(val);
+        return mtr_ * gtr_ / (cosrho * cosrho * s_);
+    }
+
+    double qscale(const double val) const {
+        return std::sqrt(hats(val));
+    }
+
+    double hats(const double val) const {
+        return mtr_ * gtr_ * std::tan(val) + mtr_ * mtr_;
+    }
+
+    friend double rho_value(const Rho &rho);
+};
+
+inline double rho_value(const Rho &rho) {
+    return rho.rho1_ + getRandom() * (rho.rho2_ - rho.rho1_);
+}
+}  // namespace pmc
+
+#endif  // SRC_HADRONS_H_
--- src/ppZGmumu.cc
+++ src/ppZGmumu.cc
@@ -1,10 +1,150 @@
+#include <cmath>
+#include <cstdlib>
 #include <iostream>
-// #include "LHAPDF/LHAPDF.h"
-#include "LHAPDF/PDFSet.h"
+#include <memory>
+#include <string>
+// #include "LHAPDF/Info.h"  // for LHAPDF::getConfig
+#include "LHAPDF/LHAPDF.h"
+#include "constants.h"
+#include "hadrons.h"
+#include "utils.h"
 
-int main() {
-    // const LHAPDF::PDF *pdf = LHAPDF::mkPDF("cteq6l1", 0);
-    const LHAPDF::PDFSet set("cteq6l1");
-    std::cout << "SetDesc: " << set.get_entry("SetDesc") << '\n';
-    std::cout << "Verbosity from set: " << set.get_entry("Verbosity") << '\n';
+const int N = 1000000;
+const double QMIN = 60.0;
+const double MTR = QMIN;
+const double GTR = QMIN;
+
+/** scale for the pdfs */
+const double MU = MZ;
+
+double weight(std::shared_ptr<LHAPDF::PDF> pdf, const double hats,
+              const double mu, const double x1, const double x2,
+              const double costh);
+
+int main(int argc, char *argv[]) {
+    std::string appname("ppZGmumu");
+    if (argc != 3) {
+        std::cerr << "Usage: " << appname << " ecm nevent\n";
+        return 1;
+    }
+
+    const double eCM = std::atof(argv[1]);
+    std::cout << "** p p --> Z/gamma --> mu+ mu- (ECM = " << eCM << " GeV)\n";
+
+    /*
+     * First step: we calculate:
+     * - the cross section
+     * - and the maximum point of the phase space
+     */
+    double sum_w = 0, sum_w_sq = 0;  // for the variance
+    double w_max = 0;
+    double costh_max = -2;
+    double q_max = -1;
+
+    const double s = eCM * eCM;
+    const pmc::Rho rho = pmc::Rho(QMIN, MTR, GTR, s);
+
+    // LHAPDF::Info &cfg = LHAPDF::getConfig();
+    // cfg.set_entry("Verbosity", 0);  // make lhapdf quiet
+    std::shared_ptr<LHAPDF::PDF> pdf(LHAPDF::mkPDF("cteq6l1"));
+
+    std::cout << "-- Integrating for cross section and getting maximum ...\n";
+    for (int i = 0; i != N; ++i) {
+        pmc::printProgress(i, N);
+
+        // random costh and rho
+        double costh = -1.0 + pmc::getRandom() * DELTATH;
+        double rho_val = pmc::rho_value(rho);
+        double hats = rho.hats(rho_val);
+
+        pmc::InitQuark qin = pmc::InitQuark(s, hats);
+        double x1 = qin.x1(), x2 = qin.x2();
+
+        // calculate the phase space point
+        double w = weight(pdf, hats, MU, x1, x2, costh) * DELTATH *
+                   rho.delta() * qin.delta_y() * rho.jacobian(rho_val) /
+                   (x1 * x2);
+        sum_w += w;  // add to the sums
+        sum_w_sq += w * w;
+        if (w > w_max) {  // check if higher than maximum
+            w_max = w;
+            costh_max = costh;
+            q_max = std::sqrt(hats);
+        }
+    }
+
+    // cross section
+    const double sigma = sum_w / N;
+
+    // calculate the error
+    const double variance = sum_w_sq / N - std::pow(sum_w / N, 2);
+    const double error = std::sqrt(variance / N);
+
+    std::cout << "---- Done integrating.\n";
+    std::cout << "---- Maximum value of dsigma = " << w_max
+              << ", found at costh = " << costh_max << '\n';
+    std::cout << "---- Total cross section = " << sigma * PBCONV << " +- "
+              << error * PBCONV << " pb\n";
+
+    /*
+     * Second step: we generate events
+     *
+     * We must boost from the CM frame to LAB frame.
+     */
+}
+
+double dsigma(const double costh, const double hats, const pmc::Qtype typ) {
+    const double kappa = std::sqrt(2) * GF * MZ2 / (4 * PI * ALPHA);
+    const double fBW = pmc::fBreitWignerZ(hats);
+    const double chi1 = kappa * hats * (hats - MZ2) * fBW;
+    const double chi2 = kappa * kappa * hats * hats * fBW;
+
+    // CL and CR for leptons
+    const double cVl = -0.5 + 2 * SW2;
+    const double cAl = -0.5;
+
+    // CL and CR for quarks
+    double cVf, cAf, qf;
+    if (typ == pmc::Qtype::UP) {  // up-type quarks
+        cVf = 0.5 - 4.0 * SW2 / 3;
+        cAf = 0.5;
+        qf = 2.0 / 3;
+    } else {  // down-type quarks
+        cVf = -0.5 + 2.0 * SW2 / 3;
+        cAf = -0.5;
+        qf = -1.0 / 3;
+    }
+
+    const double a0 = qf * qf - 2 * qf * cVl * cVf * chi1 +
+                      (cVl * cVl + cAl * cAl) * (cVf * cVf + cAf * cAf) * chi2;
+    const double a1 =
+        -4 * qf * cAl * cAf * chi1 + 8 * cAl * cVl * cAf * cVf * chi2;
+    //  1/3 from initial color averaging
+    const double prefac = 2 * PI * ALPHA * ALPHA / (4 * hats) / 3;
+
+    return prefac * (a0 * (1 + costh * costh) + a1 * costh);
+}
+
+double weight(std::shared_ptr<LHAPDF::PDF> pdf, const double hats,
+              const double mu, const double x1, const double x2,
+              const double costh) {
+    // up-type quarks
+    pmc::Qtype typ = pmc::Qtype::UP;
+    double w = dsigma(costh, hats, typ) *
+               (pdf->xfxQ(2, x1, mu) * pdf->xfxQ(-2, x2, mu) +
+                pdf->xfxQ(4, x1, mu) * pdf->xfxQ(-4, x2, mu));
+    w += dsigma(-costh, hats, typ) *
+         (pdf->xfxQ(-2, x1, mu) * pdf->xfxQ(2, x2, mu) +
+          pdf->xfxQ(-4, x1, mu) * pdf->xfxQ(4, x2, mu));
+
+    // down-type quarks
+    typ = pmc::Qtype::DOWN;
+    w += dsigma(costh, hats, typ) *
+         (pdf->xfxQ(1, x1, mu) * pdf->xfxQ(-1, x2, mu) +
+          pdf->xfxQ(3, x1, mu) * pdf->xfxQ(-3, x2, mu));
+    w += dsigma(-costh, hats, typ) *
+         (pdf->xfxQ(-1, x1, mu) * pdf->xfxQ(1, x2, mu) +
+          pdf->xfxQ(-3, x1, mu) * pdf->xfxQ(3, x2, mu));
+
+    return w;
 }
